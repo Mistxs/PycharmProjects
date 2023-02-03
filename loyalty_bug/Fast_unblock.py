6b68d1ea0f30660f64f556936125ad2ee0062456
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-02-03 23:56:55 +0300

    update from PC
    Work version, added Flask

--- loyalty_bug/Fast_unblock.py
+++ loyalty_bug/Fast_unblock.py
@@ -1,4 +1,57 @@
 import requests
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'здрасьте вам'
+
+# allow both GET and POST requests
+@app.route('/unlock', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    cnt = 0
+    if request.method == 'POST':
+        inputdata = request.form.get('URL')
+        cnt += 1
+        salon_and_rec = clearurl(inputdata)
+        print("POST #", cnt)
+        print("salon_id, rec_id:")
+        print(salon_and_rec[0], salon_and_rec[1])
+        print("document, date:")
+        document = get_document(salon_and_rec[0], salon_and_rec[1])
+        print(document)
+        print("kkm_id:")
+        kkm_id = get_kkm_id(salon_and_rec[0], document[0], document[1], document[2])
+        print(kkm_id)
+        print("summary: ")
+        print(f"UPDATE documents SET bill_print_status = 1 WHERE id = {document[0]};")
+        print(f"UPDATE kkm_transactions SET status = 3 WHERE id = {kkm_id};")
+
+        return '''<h1>OK</h1>
+                  <p>UPDATE documents SET bill_print_status = 1 WHERE id = {0};</p>
+                  <p>UPDATE kkm_transactions SET status = 3 WHERE id = {1};</p>
+                  <a href = "/unlock"> Обновить страницу  </a>
+                  '''.format(document[0], kkm_id)
+
+
+    return '''
+              <form method="POST">
+              <p> Ссылка в виде https://yclients.com/timetable/493251#main_date=2023-01-28&open_modal_by_record_id=569963743 
+              <b>ОБЯЗАТЕЛЬНО</b> </p>
+                  <div>Вставь ссылку на запись <input type="text" name="URL"></label></div>
+                  <input type="submit" value="Submit">
+              </form>'''
+
+def clearurl(data):
+    # print("URL IS: ", data)
+    newstr=data.split("/")
+    newstr[-1]=newstr[-1].split("#")
+    salon_id = int(newstr[-1][0])
+    rec_id_dirt = newstr[-1][-1].split("=")
+    rec_id = int(rec_id_dirt[-1])
+    return salon_id, rec_id
 
 def get_document(salon_id, record_id):
     url = f"https://api.yclients.com/api/v1/record/{salon_id}/{record_id}"
@@ -16,14 +69,18 @@ def get_document(salon_id, record_id):
 
     document = response_clean["data"]["documents"][0]["id"]
     c_date = response_clean["data"]["create_date"]
+    v_date = response_clean["data"]["datetime"]
     c_date_pretty = c_date.split("T")
+    v_date_pretty = v_date.split("T")
     new_date = c_date_pretty[0]
+    end_date = v_date_pretty[0]
     # print(document, new_date)
-    return document, new_date
+    return document, new_date, end_date
 
-def get_kkm_id(salon_id, doc_id, date):
 
-    url = f"https://api.yclients.com/api/v1/kkm_transactions/{salon_id}?start_date={date}&end_date={date}&editable_length=100"
+def get_kkm_id(salon_id, doc_id, date, enddate):
+
+    url = f"https://api.yclients.com/api/v1/kkm_transactions/{salon_id}?start_date={date}&end_date={enddate}&editable_length=1000"
     payload = {}
     headers = {
         'Accept': 'application/vnd.yclients.v2+json',
@@ -33,15 +90,34 @@ def get_kkm_id(salon_id, doc_id, date):
     }
     response = requests.request("GET", url, headers=headers, data=payload)
     pretty_response = response.json()
-    kkm_id = 0
+    kkm_id = 1
+    cnt = 0
+    listofid = []
     for i in range(len(pretty_response["data"])):
         if pretty_response["data"][i]["document_id"] == doc_id:
-            kkm_id = pretty_response["data"][i]["id"]
-    return kkm_id
+            # print("Step ", i)
+            # print(cnt, kkm_id, listofid)
+            cnt += 1
+            if cnt > 1:
+                listofid.append(pretty_response["data"][i]["id"])
+            else:
+                kkm_id = pretty_response["data"][i]["id"]
+    listofid.append(kkm_id)
+    max_kkm = max(listofid)
+    return max_kkm
+
+#
+# document = get_document(salon_id, 572948924)
+# kkm_id = get_kkm_id(764917,document[0],document[1])
+#
+# print(f"UPDATE documents SET bill_print_status = 1 WHERE id = {document[0]};")
+# print(f"UPDATE kkm_transactions SET status = 3 WHERE id = {kkm_id};")
 
 
-document = get_document(764917, 572948924)
-kkm_id = get_kkm_id(764917,document[0],document[1])
 
-print(f"UPDATE documents SET bill_print_status = 1 WHERE id = {document[0]};")
-print(f"UPDATE kkm_transactions SET status = 3 WHERE id = {kkm_id};")
+if __name__ == '__main__':
+    app.run()
+
+
+
+
