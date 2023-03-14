4f88ab64fc4139cf64dd4507690e4aa0e6a4f8f8
Mistxs <96961160+Mistxs@users.noreply.github.com>
2023-03-14 13:25:55 +0300

    logs upd

--- loyalty_bug/selenium_test.py
+++ loyalty_bug/selenium_test.py
@@ -5,45 +5,47 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 asd = webdriver.Chrome()
-link = 'https://www.yclients.com/signin/'
-login = "a.filippov@yclients.tech"
-password = "Ose7vgt5"
-link2 = 'https://yclients.com/documents/goodstransactions/694561/0/?type=3'
+link = 'https://yclients.com/timetable/331981#main_date=2023-03-13&mode=1'
+# login = "a.filippov@yclients.tech"
+# password = "Ose7vgt5"
+# link2 = 'https://yclients.com/documents/goodstransactions/694561/0/?type=3'
 code = '1.207576359389E'
+cookie = {'name': 'auth', 'value': 'u-12216686-09203fd795b447fc8c56b'}
+asd.add_cookie(cookie)
 
 try:
     asd.get(link)
     asd.maximize_window()
-    button = WebDriverWait(asd, 5).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, '[type="submit"]')))
-    logn = asd.find_element(By.CSS_SELECTOR, "[name='email']")
-    logn.click()
-    logn.send_keys(login)
-    paswd = asd.find_element(By.CSS_SELECTOR, '[type="password"]')
-    paswd.click()
-    paswd.send_keys(password)
-    button.click()
-    time.sleep(4)
+    # button = WebDriverWait(asd, 5).until(
+    #     EC.element_to_be_clickable((By.CSS_SELECTOR, '[type="submit"]')))
+    # logn = asd.find_element(By.CSS_SELECTOR, "[name='email']")
+    # logn.click()
+    # logn.send_keys(login)
+    # paswd = asd.find_element(By.CSS_SELECTOR, '[type="password"]')
+    # paswd.click()
+    # paswd.send_keys(password)
+    # button.click()
+    # time.sleep(4)
 
 
-    asd.get(link2)
-    min_sbar = asd.find_element(By.ID, 'super-panel-close-btn')
-    min_sbar.click()
-    add = WebDriverWait(asd, 5).until(EC.element_to_be_clickable(
-        (By.ID, 'add_good_transaction')))
-    add.click()
-    inp = asd.find_element(
-        By.CSS_SELECTOR, '[class="form-control ui-autocomplete-input"][data-num="1"]')
-    inp.click()
-    inp.send_keys(code)
-    time.sleep(2)
-    inp2 = asd.find_element(
-        By.CSS_SELECTOR, '[class="form-control ui-autocomplete-input"][data-num="2"]')
-    inp2.click()
-    for i in range(10000):
-        inp2.send_keys(code)
-        time.sleep(0.1)
-    time.sleep(5)
+    # asd.get(link2)
+    # min_sbar = asd.find_element(By.ID, 'super-panel-close-btn')
+    # min_sbar.click()
+    # add = WebDriverWait(asd, 5).until(EC.element_to_be_clickable(
+    #     (By.ID, 'add_good_transaction')))
+    # add.click()
+    # inp = asd.find_element(
+    #     By.CSS_SELECTOR, '[class="form-control ui-autocomplete-input"][data-num="1"]')
+    # inp.click()
+    # inp.send_keys(code)
+    # time.sleep(2)
+    # inp2 = asd.find_element(
+    #     By.CSS_SELECTOR, '[class="form-control ui-autocomplete-input"][data-num="2"]')
+    # inp2.click()
+    # for i in range(10000):
+    #     inp2.send_keys(code)
+    #     time.sleep(0.1)
+    # time.sleep(5)
 
 finally:
     asd.quit()
